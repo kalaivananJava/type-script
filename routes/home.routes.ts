5918645722f41cc5ed70cc03a2c67bb84084efc2
kalaivananJava <skv82202@gmail.com>
2024-10-28 12:27:38 +0530

    type script

---
+++ routes/home.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { welcome } from "../controllers/home.controller";
+
+
+class HomeRoutes {
+  router = Router();
+
+  constructor() {
+    this.intializeRoutes();
+  }
+
+  intializeRoutes() {
+    this.router.get("/", welcome);
+
+  }
+}
+
+export default new HomeRoutes().router;
